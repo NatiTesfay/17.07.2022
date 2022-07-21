79a086070ab4c70c9f4ec080dbc2afda449965f7
NatiTesfay <netaneltes66@gmail.com>
2022-07-21 12:51:29 +0300

    1

--- main.ts
+++ main.ts
@@ -286,6 +286,7 @@
 // arrayFunc([10,"luffy"])
 
 19.
+
 // function funcArray(numArray:number[],someT:String):any{
 //     switch(someT){
 //         case "reverse":
@@ -299,6 +300,223 @@
 //     }
 // }
 // console.log(funcArray([2,3,42,5],"ONE PIECE"));
+
+21.
+
+// function namesDeatails(fName:string,lName?:string):void{
+   
+//      lName ? fName + lName :0
+//      console.log(fName + lName)
+// }
+// namesDeatails("dalia","zagaya")
+
+
+22.
+// function userDeatails(fullName:string,email?:void){
+//    email ? fullName + email :0
+//    console.log(fullName + email)
+// }
+// userDeatails("dalia zagaya" , "netaneltes66@gmail.com")
+
+23.
+
+// function catName(name:string,age?:number,kind?:string):void{
+//   age ? name + age + kind:0
+//   kind ? name + age + kind:0
+//   console.log(name + age + kind)
+
+// }
+// catName(" dalia ", 23 ," havesha ")
+
+24.
+
+// function getText(text:string,someBollean:boolean=true):any{
+// return someBollean ? `hello ${text}`:`wellcome ${text}`;
+// }
+// console.log(
+// getText(`dalia`,true))
+
+25.
+
+// function userDeatails(fName:string,lName:string,text:string="miss"):string{
+//     return text ? ` ${fName} ${lName} ${text}`
+// }
+// console.log(userDeatails("dalia" , "zagaya"))
+
+26.
+
+
+
+
+34.
+
+// class Car{
+//     compeny:string;
+//     kind:string;
+//     color:string;
+//     constructor(compeny:string,kind:string,color:string){
+//         this.compeny=compeny;
+//         this.kind=kind;
+//         this.color=color;
+//     }
+// }
+
+35.
+
+// class Elephent {
+//     heigh:string;
+//     kind:string;
+//     age:number;
+//     country:string
+//     constructor(heigh:string,kind:string,age:number,country:string){
+//         this.heigh=heigh;
+//         this.kind=kind;
+//         this.age=age;
+//         this.country=country;   
+//     }
+//     get animalDetails(){
+//         return `${this.heigh} ${this.kind} ${this.age} ${this.country}`
+//     }
+// }
+
+36.
+
+// class Pants{
+//     size:string;
+//     color:string;
+//     kind:string;
+//     lenght:number;
+//     avalible:string
+//    constructor(size:string,color:string,kind:string,lenght:number,avalible:string){
+//        this.size=size;
+//        this.color=color;
+//        this.kind=kind;
+//        this.lenght=lenght;
+//        this.avalible=avalible;
+//     }
+
+//   private get pantsDeatails():string{
+//         return `${this.size} ${this.color} ${this.kind} ${this.lenght} ${this.avalible}`
+//     }
+
+// }
+
+
+37.
+
+// class Ahtelet{
+//     fullName:string;
+//     birthDate:number;
+//     sport:string;
+//     gender?:string;
+//     constructor(fullName:string,birthDate:number,sport:string,gender:string){
+//         this.fullName=fullName;
+//         this.birthDate=birthDate;
+//         this.sport=sport;
+//         this.gender=gender;
+//     }
+//     protected atheletdeatails():any{
+//         return`${this.fullName} ${this.birthDate} ${this.sport} ${this.gender}`
+
+//     }
+   
+// }
+
+
+38.
+
+// class FootballPlayer extends Ahtelet{
+//     teamNames:String;
+//     leauge:string;
+//     constructor ( fullName:string,birthDate:number,sport:string,gender:string,teamNames:String,leauge:string){
+//         super(fullName,birthDate,sport,gender)
+//         this.teamNames=teamNames;
+//         this.leauge=leauge
+//     }
+//     // let football = new FootballPlayer("Real Madrid " , "Champions")
+
+//      footballDeatals(){
+//         return `${super.atheletdeatails()} ${this.teamNames} ${this.leauge}`
+//     }
+    
+    
+// }
+
+39.
+// class User{
+//     fName:String;
+//     lName:String;
+//     age:number;
+//     userPassword:number;
+//    protected conect:boolean;
+//     constructor( fName:String,lName:String,age:number,userPassword:number, conect:boolean){
+//         this.fName=fName;
+//         this.lName=lName;
+//         this.age=age;
+//         this.userPassword=userPassword;
+//         this. conect= conect;
+//     }
+//      get Deatail():boolean{
+//         return this.conect
+//     }
+//     set Deatails(answer:boolean){
+//         this.conect=answer
+//     }
+    
+
+//     static unKonwnAge(y_or_n:string,...someObjects:User[]){
+//        if(y_or_n == "younger"){
+//         return  someObjects.sort((a:User,b:User)=>a.age-b.age)[0]
+//        }
+//        else{
+//          return  someObjects.sort((a:User,b:User)=>b.age-a.age)[0] 
+//        }
+      
+
+//     }
+  
+// }
+
+40.
+
+// class Student extends User{
+//     birthDate:number;
+//     email:any;
+//     constructor( fName:String,lName:String,age:number,userPassword:number, conect:boolean,bitrhDate:number,email:any){
+//         super( fName,lName,age,userPassword, conect)
+//         this.birthDate=bitrhDate;
+//         this.email=email;
+//     }
+//      studentDeatails(){
+//         return  ` ${this.fName} ${this.lName} ${this.age} ${this.userPassword} ${this.conect}`
+//     }
+
+//       userConect() {
+//     if (this.conect === true) {
+//       return "green"
+//     }
+//     else if (this.conect === false) {
+//       return "red"
+//     }
+//     return
+// }
+// }
+// let some1 = new Student("neanel","tesfay","true","sd", 25,"25/05/1997","netaneltes66@gmail.com");
+// let some2 = new Student("neanel","tesfay","true","sd", 25,"25/05/1997","netaneltes66@gmail.com");
+// let some3 = new Student("neanel","tesfay","true","sd", 25,"25/05/1997","netaneltes66@gmail.com");
+// let some4 = new Student("neanel","tesfay","true","sd", 25,"25/05/1997","netaneltes66@gmail.com");
+
+// console.log(Student.unKonwnAge(some1,some2,some3,some4));
+
+
+41.
+
+
+
+
+
+
+
 
 
 
